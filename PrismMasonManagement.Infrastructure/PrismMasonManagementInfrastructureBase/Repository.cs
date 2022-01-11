ae7bcae89fb2b8e2b77809dcdb8d37ec7b244e7e
Lalit Chougule <P7112453@ness.com>
2022-01-11 20:20:10 +0530

    Repository

---
+++ PrismMasonManagement.Infrastructure/PrismMasonManagementInfrastructureBase/Repository.cs
@@ -0,0 +1,20 @@
+﻿using PrismMasonManagement.Core.PrismMasonManagementCoreBase.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismMasonManagement.Infrastructure.PrismMasonManagementInfrastructureBase
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly PrismMasonManagementDbContext _context;
+
+        public Repository(PrismMasonManagementDbContext context)
+        {
+            _context = context;
+
+        }
+    }
+}
